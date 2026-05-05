f62ab970e20ebd4bf30abcef171099877d3b12bd
Jessie Gautherot <jessie.gautherot@gmail.com>
2026-05-05 18:40:09 +0200

    feat(certification) : add entity and repository

---
+++ src/Entity/Certification.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use App\Entity\Traits\TimestampableTrait;
+use App\Entity\Traits\BlameableTrait;
+
+/**
+ * Class Certification
+ *
+ * Represents a certification obtained by a user for a theme.
+ */
+#[ORM\Entity]
+#[ORM\HasLifecycleCallbacks]
+class Certification implements BlameableInterface
+{
+    use TimestampableTrait;
+    use BlameableTrait;
+
+    #[ORM\Id, ORM\GeneratedValue, ORM\Column]
+    private ?int $id = null;
+
+    /**
+     * User who owns the certification
+     */
+    #[ORM\ManyToOne(targetEntity: User::class)]
+    #[ORM\JoinColumn(nullable: false)]
+    private User $user;
+
+    /**
+     * Theme associated to the certification
+     */
+    #[ORM\ManyToOne(targetEntity: Theme::class)]
+    #[ORM\JoinColumn(nullable: false)]
+    private Theme $theme;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUser(): User
+    {
+        return $this->user;
+    }
+
+    public function setUser(User $user): self
+    {
+        $this->user = $user;
+        return $this;
+    }
+
+    public function getTheme(): Theme
+    {
+        return $this->theme;
+    }
+
+    public function setTheme(Theme $theme): self
+    {
+        $this->theme = $theme;
+        return $this;
+    }
+}
